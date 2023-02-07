b6634be5f9f997405bd04e0c860e1c67b895e943
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-06 20:33:13 -0600

    commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,17 +5,21 @@ export const Login = () => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
    
-    const handleSubmit = () => {
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
     }
    
     return (
+        <>
         <from onSubmit={handleSubmit}>
             <label for="email">email</label>
             <input value={email} type="email" placeholder="nasim.qureshi@gmail.com" id="email" name="email" />
             <label for="password">Password</label>
             <input value={pass} type="password" placeholder="your password" id="password" name="password" />
-            <button>Log In</button>
+            <button type="submit">Log In</button>
         </from>
+        <button>Don't have an accout? Register here</button>
+        </>
     )
 }
