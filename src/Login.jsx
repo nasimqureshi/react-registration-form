d3b82353ec9af8256d3ede7f7af593aa334150ae
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-06 22:09:28 -0600

    commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -15,11 +15,11 @@ export const Login = (props) => {
         <div className="auth-form-container">
         <from className="login-form"  onSubmit={handleSubmit}>
             <label htmlFor="name">Full Name</label>
-            <input value={name} name="name" id="name" placeholder="full name" />
-            <label htmlFor="email">email</label>
-            <input value={email} type="email" placeholder="nasim.qureshi@gmail.com" id="email" name="email" />
+            <input value={name} name="name" id="name" placeholder="Eneter your full name" />
+            <label htmlFor="email">Email</label>
+            <input value={email} type="email" placeholder="Enter your email address" id="email" name="email" />
             <label htmlFor="password">Password</label>
-            <input value={pass} type="password" placeholder="your password" id="password" name="password" />
+            <input value={pass} type="password" placeholder="Enter your password" id="password" name="password" />
             <button type="submit">Log In</button>
         </from>
         <br />
